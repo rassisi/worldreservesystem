764fdb6a6dc53aac56a914e6951eea40d38e5282
Ramin <Ramin@DESKTOP-69V2J7P>
2018-04-06 20:31:29 +0200

    Google Auth wrapper

---
+++ org.worldreservesystem.core/src/org/worldreservesystem/core/WRS.java
@@ -0,0 +1,15 @@
+package org.worldreservesystem.core;
+
+/**
+ * @author Ramin
+ *
+ */
+public interface WRS {
+
+	String WORLD_RESERVE_SYSTEM = "WorldReserveSystem";
+
+	String WORLD_RESERVE_SYSTEM_WEB_SITE = WORLD_RESERVE_SYSTEM + ".com";
+
+	String ISSUER = WORLD_RESERVE_SYSTEM;
+	
+}
