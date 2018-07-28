4e127eb0b181e5a9b30776672a906b287d34ca3e
ramin <ramin@LAPTOP-FPJQM4GQ.Speedport_W_724V_Typ_A_05011603_05_028>
2018-07-28 22:16:30 +0200

    a

---
+++ org.wrs.model/src-gen/org/wrs/model/wrs/impl/StatisticsSourceImpl.java
@@ -0,0 +1,49 @@
+/**
+ */
+package org.wrs.model.wrs.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.internal.cdo.CDOObjectImpl;
+
+import org.wrs.model.wrs.StatisticsSource;
+import org.wrs.model.wrs.WrsPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Statistics Source</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class StatisticsSourceImpl extends CDOObjectImpl implements StatisticsSource {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected StatisticsSourceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return WrsPackage.Literals.STATISTICS_SOURCE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected int eStaticFeatureCount() {
+		return 0;
+	}
+
+} //StatisticsSourceImpl
