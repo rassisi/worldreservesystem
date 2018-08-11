193dc16e3356999963a6b3a7053db40005105dcb
ramin <ramin@LAPTOP-FPJQM4GQ.Speedport_W_724V_Typ_A_05011603_05_028>
2018-08-12 01:17:26 +0200

    a

--- org.wrs.rcpl.ui.application/src/org/wrs/rcpl/ui/application/WrsToolFactory.java
+++ org.wrs.rcpl.ui.application/src/org/wrs/rcpl/ui/application/WrsToolFactory.java
@@ -3,13 +3,13 @@ package org.wrs.rcpl.ui.application;
 import org.eclipse.rcpl.ITool;
 import org.eclipse.rcpl.model_2_0_0.rcpl.Tool;
 
-import com.joffice.rcpl.addon.office.internal.impl.JOfficeToolFactory;
+import com.joffice.rcpl.addon.office.internal.impl.JOToolFactory;
 
 /**
  * @author ramin
  *
  */
-public class WrsToolFactory extends JOfficeToolFactory {
+public class WrsToolFactory extends JOToolFactory {
 
 	@Override
 	public ITool createTool(final Tool eTool) {
